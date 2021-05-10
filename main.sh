58a57f13374b84004dc2f94e287d8e1ebbd74bb4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-05-11 02:48:28 +0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,16 @@
+echo 'Add repository'
 sudo add-apt-repository -y ppa:gezakovacs/ppa
+echo 'apt-get update'
 sudo apt-get update
+echo 'Install make'
 sudo apt-get install make
+echo 'Install ansible'
 make install_ansible
+echo 'Set needed permissions for ansible'
 sudo chown -R $USER ~/.ansible/
+echo 'Install environment'
 make install_environment
+echo 'Configurate VIM'
 make update_vim
+echo 'Configurate aliases'
 make update_aliases
