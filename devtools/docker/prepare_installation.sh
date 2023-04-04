c49da04c10c43268b045f7a23fe3699e2d8ed016
Pasha Kalashnikov <kalashnikovisme@gmail.com>
2023-04-04 21:06:16 +0400

    Installation of the last version of docker

---
+++ devtools/docker/prepare_installation.sh
@@ -0,0 +1,4 @@
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
