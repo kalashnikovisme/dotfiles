95ff65ba3d0bf9931c7fb5b2636eb763fbe289e4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-09-14 17:35:35 +0400

    Install Docker only on Ubuntu

--- main.sh
+++ main.sh
@@ -46,10 +46,12 @@ sudo chown -R $USER ~/.ansible/
 echo 'Install environment'
 OS=$OS make install_environment && make update_vim && make update_bash
 
-echo 'Add current user to docker group'
-sudo usermod -a -G docker $USER
+if [ "$OS" = "ubuntu" ]; then
+  echo 'Add current user to docker group'
+  sudo usermod -a -G docker $USER
 
-echo 'Change docker owner to current user'
-sudo chown $USER:$USER /var/run/docker.sock
+  echo 'Change docker owner to current user'
+  sudo chown $USER:$USER /var/run/docker.sock
 
-echo 'NOW, LOG OUT AND LOG BACK TO APPLY DOCKER CHANGES'
+  echo 'NOW, LOG OUT AND LOG BACK TO APPLY DOCKER CHANGES'
+fi
