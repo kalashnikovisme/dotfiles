a5087c9cf193c6c75a4a294fdf55c4a030d5b9a8
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-09-15 23:30:09 +0400

    Enable relative line numbers for Omarchy LazyVim (#57)

--- files/omarchy/init.lua
+++ files/omarchy/init.lua
@@ -1,6 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- Disable line wrapping by default
+vim.opt.wrap = false
+
+-- Enable relative line numbers like the init.vim configuration
+vim.opt.relativenumber = true
+
 -- Neovim key mappings for Omarchy installation
 -- F2: write, F3: quit, F4: write and quit
 
